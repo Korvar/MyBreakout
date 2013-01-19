059588a4c5eeccba9ba660408cbf71e146f1a237
Korvar <korvarthefox@gmail.com>
2013-01-19 14:49:15 +0000

    Added Bat class
    Bat class added, tracks the mouse.

---
+++ MyBreakout/src/com/cugley/MyBreakout/Bat.java
@@ -0,0 +1,30 @@
+package com.cugley.MyBreakout;
+
+import org.flixel.FlxObject;
+import org.flixel.FlxSprite;
+import org.flixel.FlxG;
+
+public class Bat extends FlxSprite 
+{
+	
+	public int _max_health;
+	
+	public Bat(int X, int Y, int XVel, int YVel)
+	{
+		_max_health = 5;
+		health = _max_health;
+		FlxG.createBitmap(32,  8,  0xFFFFFF);
+		origin.x = width / 2;
+		origin.y = height / 2;
+	}
+
+	@Override public void update()
+	{
+		x = FlxG.mouse.x - width / 2;
+		if (x < 0) x = 0;
+		if (x> FlxG.width - width) x = FlxG.width - width;
+		y = FlxG.height - 24;
+			
+		super.update();
+	}
+}
--- MyBreakout/src/com/cugley/MyBreakout/PlayState.java
+++ MyBreakout/src/com/cugley/MyBreakout/PlayState.java
@@ -6,13 +6,22 @@ import org.flixel.*;
 public class PlayState extends FlxState
 {
 	private Ball _ball;
+	private Bat _bat;
+	private FlxGroup _block;
+	private FlxText _scoreDisplay;
 	
 	
 	@Override
 	public void create()
 	{
+		// Ball
 		add(new FlxText(0, 0, 200, "Hello World"));
 		_ball = new Ball(FlxG.width / 2, FlxG.height /2, 100, -100);
 		add(_ball);
+		
+		// Bat
+		_bat = new Bat((int)FlxG.mouse.x, FlxG.height - 24, 0, 0);
+		add(_bat);
+		
 	}
 }
